d25508c52e5dd49f0a2915564359361da9060df4
sam <sam@badstubner.com>
2024-01-27 11:09:44 -0700

    better background removal

--- utils/img_processing.py
+++ utils/img_processing.py
@@ -1,6 +1,8 @@
 import cv2
 from PIL import Image
 import PIL.ImageOps   
+from rembg import remove
+
 
 class Masker:
     def __init__(self, design_path, mask_path, destination_path):
@@ -12,6 +14,7 @@ class Masker:
         self._read_files()
         self._resize_design()
         self._mask()
+        # self._remove_bg()
         self._make_transparent()
         self._write_result()
 
@@ -27,13 +30,18 @@ class Masker:
 
     def _make_transparent(self):
         tmp = cv2.cvtColor(self.result, cv2.COLOR_BGR2GRAY)
-        _,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+        _,alpha = cv2.threshold(tmp, 10, 255, cv2.THRESH_BINARY)
 
         b, g, r = cv2.split(self.result)
         rgba = [b,g,r, alpha]
         self.result = cv2.merge(rgba,4)
+        
+    def _remove_bg(self):
+        self.result = remove(self.result)
+
 
     def _write_result(self):
+
         cv2.imwrite(self.destination_path, self.result)
 
 
